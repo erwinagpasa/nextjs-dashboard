2aaadecb1bb4d062ecf4ab9fef4c31f7722b26d4
erwinagpasa <istran.net@gmail.com>
2022-12-19 14:21:31 +0300

    fixing

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,7 +11,7 @@ export const Sidebar = () => {
               </div>
             </li>
             <li>
-              <Link href="/" className="relative flex flex-row items-center h-11 focus:outline-none border-l-4 border-transparent pr-6">
+              <Link href="/" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 border-l-4 border-transparent hover:border-blue-500 pr-6">
                 <span className="inline-flex justify-center items-center ml-4">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                 </span>
