449db86a1249fd9e00e0366a95927650c990afc3
erwinagpasa <istran.net@gmail.com>
2022-12-20 13:19:18 +0300

    fixing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Navbar } from '../components/Navbar'
 import { Sidebar } from '../components/Sidebar'
 import { Address } from '../widget/Address'
+import Link from 'next/link'
 // import { ChartWidget } from '../widget/Chart'
 import { ContactForm } from '../widget/ContacForm'
 import { ActiveOrders } from '../widget/ActiveOrders'
@@ -20,44 +21,44 @@ export default function Home() {
       
       <div className="h-full ml-14 mt-14 mb-10 md:ml-64">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
-        <div className="bg-slate-300 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
+        <div className="bg-slate-200 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
 
-            <div className="flex justify-center items-center w-14 h-14 bg-slate-200 rounded-full transition-all duration-300 transform group-hover:rotate-12">
+            <div className="flex justify-center items-center w-14 h-14 bg-slate-100 rounded-full transition-all duration-300 transform group-hover:rotate-12">
               <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 transform transition-transform duration-500 ease-in-out">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
               </svg>
             </div>
             <div className="text-right">
               <p className="text-2xl">1,257</p>
-              <p>Visitors</p>
+              <small>Today's order</small>
             </div>
           </div>
 
-          <div className="bg-slate-300 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
-            <div className="flex justify-center items-center w-14 h-14 bg-slate-200 rounded-full transition-all duration-300 transform group-hover:rotate-12">
+          <div className="bg-slate-200 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-slate-100 rounded-full transition-all duration-300 transform group-hover:rotate-12">
               <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
             </div>
             <div className="text-right">
               <p className="text-2xl">557</p>
-              <small>Today's Orders</small>
+              <small>This month delivered</small>
             </div>
           </div>
 
-          <div className="bg-slate-300 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
-            <div className="flex justify-center items-center w-14 h-14 bg-slate-200 rounded-full transition-all duration-300 transform group-hover:rotate-12">
+          <div className="bg-slate-200 rounded-md flex items-center justify-between p-3 text-slate-700 font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-slate-100 rounded-full transition-all duration-300 transform group-hover:rotate-12">
               <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-slate-700 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
             </div>
             <div className="text-right">
-              <p className="text-2xl">11,257</p>
-              <small>Delivered</small>
+              <p className="text-2xl">$11,257</p>
+              <small>This month revenue</small>
             </div>
           </div>
 
-          <div className="bg-slate-100 rounded-md flex items-center justify-center p-3 text-slate-700 font-medium group">           
-            <div className="text-right">
-              <p className="text-2xl">$75,257</p>
-              <small>Current month sales</small>
-            </div>
+          <div className="bg-slate-100 rounded-md flex items-center justify-center pl-3 text-slate-700 font-medium group">           
+            <Link className="block w-full relative " href="/">
+            <span>Logistics Dashboard</span>              
+            <span className="block text-xs text-gray-500">Version 1.0.0 | Build 1</span>
+          </Link>
           </div>
         </div>
     
