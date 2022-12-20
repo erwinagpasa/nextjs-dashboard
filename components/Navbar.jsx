a96dfc0672934a5ac7242cbacaad5363f6ab05f7
erwinagpasa <rootaccess000@gmail.com>
2022-12-20 21:33:32 +0300

    fixing hover effects

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -38,18 +38,16 @@ export const Navbar = () => {
                 <input type="search" placeholder="Search" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
               </div>
               </div>
-
           
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" className="p-1 text-slate-800 hover:text-slate-500">
-                 
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                <button type="button" className="p-1 text-slate-800 hover:text-slate-500">                 
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex text-slate-800 text-sm focus:outline-none hover:text-gray-100">
+                    <Menu.Button className="flex text-slate-800 text-sm focus:outline-none hover:text-gray-500">
                       <span className="sr-only">Open user menu</span>
                       <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                     </Menu.Button>
@@ -66,11 +64,8 @@ export const Navbar = () => {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
+                          <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                          Your Profile
                           </a>
                         )}
                       </Menu.Item>
